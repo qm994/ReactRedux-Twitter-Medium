d7385e0a3a1ca63741e21758fe92245ee0e19d63
Qingyuan Ma <qm28@georgetown.edu>
2020-04-18 12:36:55 -0400

    reducer finish

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
-import { users } from './users';
-import { tweets } from './tweets';
-import { authedUser } from './authedUser';
+import users from './users';
+import tweets from './tweets';
+import authedUser from './authedUser';
 
 export default combineReducers({
+    authedUser,
     users,
-    tweets,
-    authedUser
-})
+    tweets
+  })
